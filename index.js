5b05b6c1997eb419c4853cf4cd728ce5e1e024b7
Somto <somtoigbokwe2017@yahoo.com>
2022-12-26 18:08:08 +0100

    my first commit

---
+++ index.js
@@ -0,0 +1,163 @@
+// window.alert("i love pizza");
+
+// let firstName = "Bro";
+// let age = 12;
+
+// console.log(firstName);
+// console.log(age + 8);
+
+// document.getElementById("p1").innerHTML = "Hello " + firstName;
+// document.getElementById("p2").innerHTML = "you are " + age + " years old";
+// let userName;
+// document.getElementById("myButton").onclick = () => {
+//   userName = document.getElementById("customer-name").value;
+//   console.log(userName);
+//   document.getElementById("head").innerHTML = "hello " + userName;
+// };
+
+// let age = window.prompt("How old are you");
+// age = Number(age);
+// age += 1;
+
+// console.log("Happy Birthday you are " + age + " old");
+
+// let x;
+// let y;
+// let z;
+
+// x = Number("pizza");
+// y = String(0);
+// z = Boolean("yam");
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+// let pi = 3.14159;
+// let radius;
+// let circumference;
+
+// radius = window.prompt("Enter the radius of a circle");
+// radius = Number(radius);
+// circumference = 2 * pi * radius;
+
+// console.log("the circumference is ", circumference);
+
+// let x = 2.14;
+// let y = 9;
+// let z = 5;
+
+// x = Math.round(x);
+// x = Math.sqrt(x);
+// x = Math.pow(x, 4);
+// x = Math.abs(x); if x is a negative value
+// x = Math.ceil(x);
+
+// x = Math.max(x, y, z);
+// x = Math.min(x, y, z);
+
+// console.log(x);
+
+let a;
+let b;
+let c;
+
+// a = window.prompt("Enter side a");
+// a = Number(a);
+
+// b = window.prompt("Enter side b");
+// b = Number(b);
+
+// c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+// console.log(c);
+
+// document.getElementById("submit").onclick = () => {
+//   a = document.getElementById("aTextBox").value;
+//   a = Number(a);
+
+//   b = document.getElementById("bTextBox").value;
+//   b = Number(b);
+
+//   c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+//   document.getElementById("cLabel").innerHTML = "C is now " + c;
+// };
+
+// let count = 0;
+
+// document.getElementById("decreasebtn").onclick = () => {
+//   count -= 1;
+//   document.getElementById("countLabel").innerHTML = count;
+// };
+
+// document.getElementById("resetbtn").onclick = () => {
+//   count = 0;
+//   document.getElementById("countLabel").innerHTML = count;
+// };
+
+// document.getElementById("increasebtn").onclick = () => {
+//   count += 1;
+//   document.getElementById("countLabel").innerHTML = count;
+// };
+
+// let x;
+// let y;
+// let z;
+
+// document.getElementById("rollButton").onclick = () => {
+//   x = Math.floor(Math.random() * 6) + 1;
+//   y = Math.floor(Math.random() * 6) + 1;
+//   z = Math.floor(Math.random() * 6) + 1;
+
+//   document.getElementById("xlabel").innerHTML = x;
+//   document.getElementById("ylabel").innerHTML = y;
+//   document.getElementById("zlabel").innerHTML = z;
+// };
+
+// let username = " Bro code ";
+// let phoneNumber = "123-458-345";
+
+// username = username.toUpperCase();
+// username = username.trim();
+// username = username.charAt(2);
+// username = username.toLowerCase();
+
+// phoneNumber = phoneNumber.replaceAll("-", "/");
+
+// console.log(phoneNumber);
+
+// let fullName = "Bro Code";
+// let firstName;
+// let lastName;
+// firstName = fullName.slice(0, fullName.indexOf(" "));
+// lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+// // lastName = fullName.indexOf(" ");
+
+// console.log(firstName);
+// console.log(lastName);
+
+// let userName = "bro";
+
+// let letter = userName.charAt(0).toUpperCase();
+
+// console.log(letter);
+
+// let age = -8;
+
+// if (age >= 18) {
+//   console.log("You are an adult");
+// } else if (age < 0) {
+//   console.log("Dem never born you");
+// } else {
+//   console.log("You are a child");
+// }
+
+// let online = false;
+
+// if (online) {
+//   console.log("you are online");
+// } else {
+//   console.log("you are offline");
+// }
